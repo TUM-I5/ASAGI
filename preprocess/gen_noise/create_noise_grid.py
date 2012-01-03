e8ddd701f1adf6005e2735fbda474d7a3f279ca7
Sebastian Rettenberger <rettenbs@in.tum.de>
2012-01-03 18:44:26 +0000

    Create nc noise files with python

--- preprocess/gen_noise/create_noise_grid.py
+++ preprocess/gen_noise/create_noise_grid.py
@@ -3,6 +3,30 @@
 import math
 from pnoise import pnoise
 
+### Try to import netcdf ##
+
+# Do some magic here, because I don't want to mess up my python installation
+import os, sys
+netcdf_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'netcdf')
+if netcdf_folder not in sys.path:
+	sys.path.insert(0, netcdf_folder)
+	
+create_nc = False
+try:
+	from netCDF3 import Dataset
+	create_nc = True
+except ImportError:
+	# Install numpy-devel files
+	# Download http://code.google.com/p/netcdf4-python/downloads/detail?name=netCDF4-0.8.2.tar.gz
+	# Extract
+	# cd netCDF4-0.8.2
+	# python setup-nc3.py build
+	# sudo python setup-nc3.py install
+	
+	import warnings
+	warnings.warn("NetCDF not found, out.nc is not created")
+
+
 #parameters
 width = 256			#width of the resulting grid [1, inf)
 height = 256		#height of the resulting grid [1, inf)
@@ -46,11 +70,23 @@ fileID.write('\n')
 fileID.write('SCALARS coeff float 1\n')
 fileID.write('LOOKUP_TABLE default\n')
 
+if create_nc:
+	nc = Dataset('out.nc', 'w')
+	nc.createDimension('x', width)
+	nc.createDimension('y', height)
+	nc_values = nc.createVariable('z', 'f4', ('x', 'y'))
+
 for j in range(0, height):
 	for i in range(0, width):
 		x = x_scaling * float(i) / float(height)
 		y = y_scaling * float(j) / float(height)
 		out = out_offset + out_scaling * noise(x, y, 0, depth, roughness)
 		fileID.write("%f\n" %out)
+		
+		if create_nc:
+			nc_values[i,j] = out
 
 fileID.close()
+
+if create_nc:
+	nc.close()
