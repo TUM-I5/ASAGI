29e1fae2ac8f1bd4992f0c99a77b8d5f01ff94a9
Sebastian Rettenberger <rettenbs@in.tum.de>
2012-03-02 16:38:02 +0000

    Add fortran interfaces to map all grid_get_xxx_nd to grid_get_xxx

--- examples/f90/simple.f90
+++ examples/f90/simple.f90
@@ -22,7 +22,7 @@ program simple
   write (*,*) "Range X:", grid_min_x( grid_id ), grid_max_x( grid_id )
   write (*,*) "Range Y:", grid_min_y( grid_id ), grid_max_y( grid_id )
 
-  write (*,*) "Value at 5x10:", grid_get_float_2d( grid_id, 5.d+0, 10.d+0 )
+  write (*,*) "Value at 5x10:", grid_get_float( grid_id, 5.d+0, 10.d+0 )
   write (*,*) "Value at 5x10.1:", grid_get_float_2d( grid_id, 5.d+0, 10.1d+0 )
   write (*,*) "Value at -1x-5005.32:", grid_get_float_2d( grid_id, -1.d+0, -5005.32d+0 )
 
