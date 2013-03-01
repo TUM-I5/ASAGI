80b56a770c762e89452ccb2fdcd4226f51a8f3f4
Sebastian Rettenberger <rettenbs@in.tum.de>
2013-03-01 10:17:25 +0100

    Removed netcd-cxx dependencies from test suite

--- tests/prepare.cpp
+++ tests/prepare.cpp
@@ -35,7 +35,7 @@
 
 #include <fstream>
 #include <iostream>
-#include <netcdf>
+#include <netcdf.h>
 
 #define NC_1D "1dgrid.nc"
 #define NC_1DPSEUDO "1dpseudogrid.nc"
@@ -48,57 +48,49 @@
 #define LENGTH 51
 #define HEIGHT 51
 
-using namespace netCDF;
-
 int main()
 {
-	NcFloat floatType;
-	NcVar var;
-	NcDim x, y, z;
+	int file;
+	int var;
+	int dim[3];
 	
 	// Create the nc files
 	
 	// 1d
-	NcFile nc_1d(NC_1D, NcFile::replace);
+	nc_create(NC_1D, NC_NETCDF4, &file);
 	
-	x = nc_1d.addDim("x", WIDTH);
+	nc_def_dim(file, "x", WIDTH, &dim[0]);
 	
-	std::vector<NcDim> dims_1d(1);
-	dims_1d[0] = x;
-	
-	var = nc_1d.addVar("z", floatType, dims_1d);
+	nc_def_var(file, "z", NC_FLOAT, 1, dim, &var);
 	
 	float values_1d[WIDTH];
 	
 	for (unsigned int i = 0; i < WIDTH; i++)
 		values_1d[i] = i;
 	
-	var.putVar(values_1d);
+	nc_put_var_float(file, var, values_1d);
+
+	nc_close(file);
 	
 	// pseudo 1d
-	NcFile nc_1dpseudo(NC_1DPSEUDO, NcFile::replace);
+	nc_create(NC_1DPSEUDO, NC_NETCDF4, &file);
+
+	nc_def_dim(file, "y", 1, &dim[0]);
+	nc_def_dim(file, "x", WIDTH, &dim[1]);
 	
-	x = nc_1dpseudo.addDim("x", WIDTH);
-	y = nc_1dpseudo.addDim("y", 1);
+	nc_def_var(file, "z", NC_FLOAT, 2, dim, &var);
 	
-	std::vector<NcDim> dims_2d(2);
-	dims_2d[0] = y;
-	dims_2d[1] = x;
+	nc_put_var_float(file, var, values_1d); // reuse values
 	
-	var = nc_1dpseudo.addVar("z", floatType, dims_2d);
-	
-	var.putVar(values_1d); // reuse values
+	nc_close(file);
 	
 	// 2d
-	NcFile nc_2d(NC_2D, NcFile::replace);
+	nc_create(NC_2D, NC_NETCDF4, &file);
 	
-	x = nc_2d.addDim("x", WIDTH);
-	y = nc_2d.addDim("y", LENGTH);
+	nc_def_dim(file, "y", LENGTH, &dim[0]);
+	nc_def_dim(file, "x", WIDTH, &dim[1]);
 	
-	dims_2d[0] = y;
-	dims_2d[1] = x;
-	
-	var = nc_2d.addVar("z", floatType, dims_2d);
+	nc_def_var(file, "z", NC_FLOAT, 2, dim, &var);
 	
 	float values_2d[LENGTH][WIDTH];
 	
@@ -106,28 +98,21 @@ int main()
 		for (unsigned int j = 0; j < WIDTH; j++)
 			values_2d[i][j] = i * WIDTH + j;
 	
-	var.putVar(values_2d);
+	nc_put_var_float(file, var, reinterpret_cast<float*>(values_2d));
+
+	nc_close(file);
 	
 	// 3d
-	NcFile file(NC_3D, NcFile::replace);
+	nc_create(NC_3D, NC_NETCDF4, &file);
 	
-	x = file.addDim("x", WIDTH);
-	y = file.addDim("y", LENGTH);
-	z = file.addDim("t", HEIGHT);	// can't set this to z ...
+	nc_def_dim(file, "t", HEIGHT, &dim[0]); // can't set this to z ...
+	nc_def_dim(file, "y", LENGTH, &dim[1]);
+	nc_def_dim(file, "x", WIDTH, &dim[2]);
 	
-	std::vector<NcDim> dims_3d(3);
-	dims_3d[0] = z;
-	dims_3d[1] = y;
-	dims_3d[2] = x;
+	nc_def_var(file, "z", NC_FLOAT, 3, dim, &var);
 	
-	var = file.addVar("z", floatType, dims_3d);
-	
-	std::vector<size_t> offset_3d(3, 0);
-	std::vector<size_t> size_3d(3);
-	
-	size_3d[0] = 1;
-	size_3d[1] = LENGTH;
-	size_3d[2] = WIDTH;
+	size_t offset_3d[3] = {0, 0, 0};
+	size_t size_3d[3] = {1, LENGTH, WIDTH};
 	
 	for (unsigned int i = 0; i < HEIGHT; i++) {
 		offset_3d[0] = i;
@@ -136,9 +121,11 @@ int main()
 			for (unsigned int k = 0; k < WIDTH; k++)
 				values_2d[j][k] = (i * LENGTH + j) * WIDTH + k;
 		
-		var.putVar(offset_3d, size_3d, values_2d);
+		nc_put_vara_float(file, var, offset_3d, size_3d, reinterpret_cast<float*>(values_2d));
 	}
 	
+	nc_close(file);
+
 	// Create the header file
 	std::ofstream headerFile;
 	headerFile.open(HEADER_FILENAME);
