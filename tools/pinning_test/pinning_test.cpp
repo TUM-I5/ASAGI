2d47acdb596e503db7711b6e8111786187bb4a62
Sebastian Rettenberger <rettenbs@in.tum.de>
2015-09-18 10:47:58 +0200

    Add tool to test thread pinning

---
+++ tools/pinning_test/pinning_test.cpp
@@ -0,0 +1,79 @@
+/**
+ * @file
+ *  This file is part of ASAGI.
+ *
+ *  ASAGI is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as
+ *  published by the Free Software Foundation, either version 3 of
+ *  the License, or  (at your option) any later version.
+ *
+ *  ASAGI is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with ASAGI.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Diese Datei ist Teil von ASAGI.
+ *
+ *  ASAGI ist Freie Software: Sie koennen es unter den Bedingungen
+ *  der GNU Lesser General Public License, wie von der Free Software
+ *  Foundation, Version 3 der Lizenz oder (nach Ihrer Option) jeder
+ *  spaeteren veroeffentlichten Version, weiterverbreiten und/oder
+ *  modifizieren.
+ *
+ *  ASAGI wird in der Hoffnung, dass es nuetzlich sein wird, aber
+ *  OHNE JEDE GEWAEHELEISTUNG, bereitgestellt; sogar ohne die implizite
+ *  Gewaehrleistung der MARKTFAEHIGKEIT oder EIGNUNG FUER EINEN BESTIMMTEN
+ *  ZWECK. Siehe die GNU Lesser General Public License fuer weitere Details.
+ *
+ *  Sie sollten eine Kopie der GNU Lesser General Public License zusammen
+ *  mit diesem Programm erhalten haben. Wenn nicht, siehe
+ *  <http://www.gnu.org/licenses/>.
+ *
+ * @copyright 2015 Sebastian Rettenberger <rettenbs@in.tum.de>
+ */
+
+#include <mpi.h>
+
+#include <omp.h>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <sched.h>
+
+int main(int argc, char* argv[])
+{
+	MPI_Init(&argc, &argv);
+
+	int rank;
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+	#pragma omp parallel
+	{
+		cpu_set_t mask;
+		sched_getaffinity(0, sizeof(cpu_set_t), &mask);
+
+		int cpus = CPU_COUNT(&mask);
+
+		std::ostringstream ss;
+		ss << "Found " << cpus << " CPUs on rank " << rank << ", thread " << omp_get_thread_num() << ": ";
+		int out = 0, current = 0;
+		while (out < cpus) {
+			if (CPU_ISSET(current, &mask)) {
+				ss << current << ", ";
+				out++;
+			}
+			current++;
+		}
+
+		std::string outString = ss.str();
+		outString = outString.substr(0, outString.size()-2);
+		std::cout << outString << std::endl;
+	}
+
+	MPI_Finalize();
+
+	return 0;
+}
